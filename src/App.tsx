a9108a3eccdb348c3db4e653437b83a86d99ecbb
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-03-24 20:03:42 -0300

    Consumindo a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { createServer } from 'miragejs';
 import { GlobalStyle } from "./styles/global";
+
+createServer({
+  routes() {
+    this.namespace = 'api' //base das rotas
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
