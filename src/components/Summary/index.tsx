ccd3fef0cc52ebf0b0a5021d4b484e3a7c18e80d
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-03-30 19:40:14 -0300

    Carregando transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,15 +1,21 @@
+import { useContext } from 'react';
+
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
+    const transactions = useContext(TransactionsContext);
+
+    console.log(transactions)
     return (
         <Container>
             <div>
                 <header>
                     <p>Entradas</p>
-                    <img src={incomeImg} alt=""/>
+                    <img src={incomeImg} alt="" />
                 </header>
                 <strong>
                     R$ 1000,00
@@ -19,7 +25,7 @@ export function Summary() {
             <div>
                 <header>
                     <p>Saídas</p>
-                    <img src={outcomeImg} alt=""/>
+                    <img src={outcomeImg} alt="" />
                 </header>
                 <strong>
                     - R$ 500,00
@@ -29,7 +35,7 @@ export function Summary() {
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
-                    <img src={totalImg} alt=""/>
+                    <img src={totalImg} alt="" />
                 </header>
                 <strong>
                     R$ 500,00
